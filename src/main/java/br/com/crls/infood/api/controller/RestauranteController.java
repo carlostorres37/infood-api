a71c10d43c93044f89810e5678b19db451ccd0c3
Carlos <carlos.cjbt3@gmail.com>
2020-01-15 22:26:11 -0300

    Implementando endpoint de atualização parcial (PATCH)

--- src/main/java/br/com/crls/infood/api/controller/RestauranteController.java
+++ src/main/java/br/com/crls/infood/api/controller/RestauranteController.java
@@ -1,13 +1,17 @@
 package br.com.crls.infood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.crls.infood.domain.exception.EntidadeNaoEncontradaException;
 import br.com.crls.infood.domain.model.Restaurante;
@@ -24,23 +30,23 @@ import br.com.crls.infood.domain.service.CadastroRestauranteService;
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
-	
+
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
+
 	@Autowired
 	private CadastroRestauranteService cadastroRestauranteService;
-	
+
 	@GetMapping
 	public List<Restaurante> listar() {
 		return restauranteRepository.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public Optional<Restaurante> buscarPorId(@PathVariable Long id) {
 		return restauranteRepository.findById(id);
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<?> salvar(@RequestBody Restaurante restaurante) {
@@ -51,7 +57,7 @@ public class RestauranteController {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-	
+
 	@PutMapping("{id}")
 	public ResponseEntity<?> atualizar(@RequestBody Restaurante restaurante, @PathVariable Long id) {
 		Optional<Restaurante> restauranteOptinal = restauranteRepository.findById(id);
@@ -62,6 +68,33 @@ public class RestauranteController {
 		}
 		return ResponseEntity.notFound().build();
 	}
-	
+
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId,
+			@RequestBody Map<String, Object> campos) {
+		Optional<Restaurante> restauranteAtual = restauranteRepository.findById(restauranteId);
+
+		if (restauranteAtual.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		merge(campos, restauranteAtual.get());
+
+		return atualizar(restauranteAtual.get(), restauranteId);
+	}
+
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+			field.setAccessible(true);
+
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		});
+	}
 
 }
